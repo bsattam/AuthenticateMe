f13c8842735ff5c8dd5b05c8ca888c8000f1ad30
Sattam <officialbsattam@gmail.com>
2022-02-07 02:03:36 +0530

    Deploying version 1.1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../styles/Footer.module.css';
-import utils from '../styles/Utilities.module.css';
 
 
 export default function Footer() {
@@ -9,5 +8,5 @@ export default function Footer() {
       text-center`} href='https://github.com/bsattam'>
       Created By: Sattam Bandyopadhyay
   </a>
-  )
+  ) 
 }
