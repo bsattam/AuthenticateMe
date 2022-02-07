d04bfb948569b85315a85e852b5f4ce07861f18c
Sattam <officialbsattam@gmail.com>
2022-02-07 12:17:39 +0530

    Deploying version 1.5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,12 @@ export default function Footer() {
     <div className={`${styles.footer} `}>
       <div className={`d-flex justify-content-evenly align-items-center m-auto ${styles.width}`}>
         <a className={`text-secondary text-center`} 
-        href='https://bsattam.github.io/Portfolio/' target="_blank">
+        href='https://bsattam.github.io/Portfolio/' target="_blank" rel="noreferrer">
           Created By: Sattam Bandyopadhyay
         </a>
         <IconContext.Provider value={{color: '#8bb4b2'}}>
-          <a href='https://www.linkedin.com/in/sattam-bandyopadhyay/' target="_blank"><BsLinkedin/></a>
-          <a href='https://github.com/bsattam' target="_blank"><BsGithub/></a>
+          <a href='https://www.linkedin.com/in/sattam-bandyopadhyay/' target="_blank" rel="noreferrer"><BsLinkedin/></a>
+          <a href='https://github.com/bsattam' target="_blank" rel="noreferrer"><BsGithub/></a>
         </IconContext.Provider>
       </div>
     </div>
